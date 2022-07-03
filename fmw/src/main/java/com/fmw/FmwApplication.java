8c08f87b3b46219042293fb80c7d3e1b6a95a499
mk <superhippoo@gmail.com>
2022-07-03 15:43:00 +0900

    test commit
    Change-Id: Ia5b8aeced83ac13e23fba4df0851765e39644f81

---
+++ fmw/src/main/java/com/fmw/FmwApplication.java
@@ -0,0 +1,13 @@
+package com.fmw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmwApplication.class, args);
+	}
+
+}
---
+++ fmw/src/main/java/com/fmw/controller/testController.java
@@ -0,0 +1,14 @@
+package com.fmw.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class testController {
+	@RequestMapping("/testcall")
+	public String test() {
+		return "testcall!!!!!!!";
+	}
+
+}

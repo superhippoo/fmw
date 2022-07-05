5f90bda6e6b6778d85c2bd2f6ee36822876f889f
mk <superhippoo@gmail.com>
2022-07-05 22:26:27 +0900

    response entity 처리
    Change-Id: I8c0cd2d7103722fe894a5dd230d4aeff16e4c361

--- fmw/src/main/java/com/fmw/ctl/userContoroller.java
+++ fmw/src/main/java/com/fmw/ctl/userContoroller.java
@@ -1,13 +1,18 @@
 package com.fmw.ctl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fmw.dto.message;
 import com.fmw.dto.userVO;
+import com.fmw.enums.statusEnum;
 import com.fmw.svc.userSvc;
 
 @RestController
@@ -18,8 +23,19 @@ public class userContoroller {
 	private userSvc usersvc;
 
 	@RequestMapping(value = "userlist", method = RequestMethod.GET)
-	public List<userVO> selectUserList() {
-		return usersvc.selectUserList();
+	public ResponseEntity<message> selectUserList() {
+		message ms = new message();
+		List<userVO> result = new ArrayList<userVO>();
+		result = usersvc.selectUserList();
+		ms.setData(result);
+		ms.setStatus(statusEnum.OK.getStatusCode());
+		ms.setTotalcount(String.valueOf(result.size()));
+		ms.setReturnmessage("Success");
+		if (result.isEmpty()) {
+			ms.setReturnmessage("Data Not Found");
+		}
+		return new ResponseEntity<message>(ms,HttpStatus.OK);
+
 	}
 
 }
---
+++ fmw/src/main/java/com/fmw/enums/statusEnum.java
@@ -0,0 +1,20 @@
+package com.fmw.enums;
+
+public enum statusEnum {
+
+	OK(200), 
+	BAD_REQUEST(400), 
+	NOT_FOUND(404), 
+	INTERNAL_SERVER_ERROR(500);
+
+	int statusCode;
+
+	statusEnum(int statusCode) {
+		this.statusCode = statusCode;
+	}
+
+	public int getStatusCode() {
+		return this.statusCode;
+	}
+
+}
